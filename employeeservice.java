f76b091aae90dc74893fdb348d708e7a695e6e66
8885444408 <chguruteja307@gmail.com>
2023-04-15 22:25:40 +0530

    intial commit

---
+++ employeeservice.java
@@ -0,0 +1,3 @@
+this file contain all the employees details
+and it contains all the the information abot the employees from the the database
+
---
+++ microsoft.java
@@ -0,0 +1,6 @@
+ in this lot of data is there regarding microsoft account 
+
+ the details mentioned about there is is all are our employeees
+
+ they all get more then one lakh salary
+
